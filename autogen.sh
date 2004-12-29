a9e275ebca682f1446b9ce20333d86fcb38008f3
sms00 <sms00>
2004-12-29 23:00:46 +0000

    Add $ACLOCAL_FLAGS to the aclocal command in order to handle nonstandard paths for the .m4 files.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 do_autogen () {
     echo "Doing autogen in $PWD... "
     echo -n "aclocal..."
-    aclocal -I /usr/local/share/aclocal
+    aclocal -I /usr/local/share/aclocal $ACLOCAL_FLAGS
     echo "done"
     echo -n "libtoolize..."
     libtoolize --automake --copy --force
