2167e5a9a1a98fde8e6e7acb1dc72eb361c16579
gmerlin <gmerlin>
2005-04-19 14:31:11 +0000

    * Everything is LGPL now

--- plugins/rtjpeg/RTjpeg.h
+++ plugins/rtjpeg/RTjpeg.h
@@ -1,35 +1,25 @@
 /* 
-   RTjpeg (C) Justin Schoeman 1998 (justin@suntiger.ee.up.ac.za)
-   
-   With modifications by:
-   (c) 1998, 1999 by Joerg Walter <trouble@moes.pmnet.uni-oldenburg.de>
-   and
-   (c) 1999 by Wim Taymans <wim.taymans@tvd.be>
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public  
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,  
-but WITHOUT ANY WARRANTY; without even the implied warranty of   
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public   
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-        Justin Schoeman:
-	  e-mail:
-        	justin@suntiger.ee.up.ac.za
-	  snail mail:
-		EE&C Engineering
-		University of Pretoria
-		Pretoria
-		0002
-		South Africa
-*/
+ * RTjpeg (C) Justin Schoeman 1998 (justin@suntiger.ee.up.ac.za)
+ *  
+ * With modifications by:
+ * (c) 1998, 1999 by Joerg Walter <trouble@moes.pmnet.uni-oldenburg.de>
+ * and
+ * (c) 1999 by Wim Taymans <wim.taymans@tvd.be>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 #ifdef HAVE_STDINT_H
 #ifndef _STDINT_H
