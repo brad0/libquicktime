8a2232717413f75f1ede0db180c7c159d8d1e0a3
sms00 <sms00>
2005-10-14 03:11:57 +0000

    Use autoreconf instead of manually running all the various auto* commands twice.

--- autogen.sh
+++ autogen.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-if test -d "/usr/local/share/aclocal"; then
-  ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
-else
-  ACLOCAL_FLAGS=""
-fi
-
-do_autogen () {
-    echo "Doing autogen in $PWD... "
-    echo -n "aclocal..."
-    aclocal -I m4 $ACLOCAL_FLAGS
-    echo "done"
-    echo -n "libtoolize..."
-    libtoolize --automake --copy --force
-    echo "done"
-    echo -n "autoheader..."
-    autoheader
-    echo "done"
-    echo -n "autoconf..."
-    autoconf
-    echo "done"
-    echo -n "automake..."
-    automake --foreign --add-missing
-    echo "done"
-}
-
-#Run this twice - rerun with auto generated files...
-do_autogen
-do_autogen
-
+autoreconf -f -i
 echo "all done. You are now ready to run ./configure"
