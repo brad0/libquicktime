f15bb213565195f1a324d007cfacdadfce7b6aba
sms00 <sms00>
2008-12-11 18:01:38 +0000

    Remove obsolete rpm spec.in file

--- cvs_clean.sh
+++ cvs_clean.sh
@@ -8,7 +8,7 @@ rm -f `find . -name "*~"`
 rm -f `find . -name "Makefile.in"`
 rm -rf configure config.* autom4te.cache
 rm -f aclocal.m4
-rm -f libquicktime.spec ltmain.sh
+rm -f ltmain.sh
 rm -f depcomp
 rm -f install.sh
 rm -f missing
